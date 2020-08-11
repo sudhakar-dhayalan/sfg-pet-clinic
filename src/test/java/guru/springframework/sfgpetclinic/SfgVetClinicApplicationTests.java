2cdf66bfa9f8666dd7eab6abba55c0dde1406a37
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2020-08-11 23:22:30 +0530

    adding POJO's. Close #2

--- src/test/java/guru/springframework/sfgpetclinic/SfgVetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgVetClinicApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+class SfgVetClinicApplicationTests {
 
     @Test
     void contextLoads() {

1711ac8615c7b3f23f56dc54bd1ee8fc0eae206f
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2020-08-16 14:02:37 +0530

    Refactor to multimodule maven project. Closes #1

---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+//@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
